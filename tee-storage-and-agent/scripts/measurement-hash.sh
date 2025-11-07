90feeec852ec233a790db767c5155315685fcc0c
oceans404 <steph.orpilla@nillion.com>
2025-11-07 14:59:43 -0800

    add nilcc and measurement scripts

---
+++ tee-storage-and-agent/scripts/measurement-hash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_PATH=$(dirname $(realpath $0))
+ROOT_PATH="$SCRIPT_PATH/../"
+DOCKER_COMPOSE_HASH=$(sha256sum $ROOT_PATH/docker-compose.nilcc.yml | cut -d" " -f1)
+VCPUS=2
+NILCC_VERSION=0.2.1
+
+MEASUREMENT_HASH=$(docker run -v/tmp/nilcc-verifier-cache:/tmp/nilcc-verifier-cache --rm ghcr.io/nillionnetwork/nilcc-verifier:latest measurement-hash $DOCKER_COMPOSE_HASH $NILCC_VERSION --vm-type cpu --cpus $VCPUS)
+echo "$MEASUREMENT_HASH"
